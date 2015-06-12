47eb5512fed6857617b4650a098941ab769afac9
Connor Taffe <cpaynetaffe@gmail.com>
2015-06-11 23:13:57 -0500

    update

--- main.go
+++ main.go
@@ -10,27 +10,19 @@ func main() {
 	if len(os.Args) != 2 {
 		fmt.Printf(os.Args[0] + " [play | pause | playpause]\n")
 	} else {
-		players, err := mpris2.Players()
+		player, err := mpris2.NewPlayer("spotify")
 		if err != nil {
 			fmt.Printf("Error: %s\n", err)
 		}
-
-		for _, player := range players {
-			fmt.Printf("Name: %s\n", player.Name)
-		}
-
-		if len(players) > 0 {
-			player := players[0]
-			switch os.Args[1] {
-			case "playpause":
-				player.PlayPause()
-			case "next":
-				player.Next()
-			case "prev":
-				player.Previous()
-			default:
-				fmt.Printf("%s is currently unsupported\n", os.Args[1])
-			}
+		switch os.Args[1] {
+		case "playpause":
+			player.PlayPause()
+		case "next":
+			player.Next()
+		case "prev":
+			player.Previous()
+		default:
+			fmt.Printf("%s is currently unsupported\n", os.Args[1])
 		}
 	}
 }
